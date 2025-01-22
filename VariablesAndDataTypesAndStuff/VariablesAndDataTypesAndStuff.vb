33e56d3e4ba1f1ebecde00314c486e895b05b906
rc-earnest <rc.earnest04@gmail.com>
2025-01-22 08:53:01 -0700

    Vars

--- VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
+++ VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
@@ -1,7 +1,30 @@
-﻿Module VariablesAndDataTypesAndStuff
+﻿'Rudy Earnest
+'Spring 2025
+'RCET 2265
+'VariablesAndDataTypesAndStuff
+'https://github.com/rc-earnest/VariablesAndDataTypesAndStuff.git
+Option Explicit On
+Module VariablesAndDataTypesAndStuff
 
     Sub Main()
 
+        Dim jellyBeans As Integer
+        Dim laffyTaffy As Integer
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = jellyBeans + 10
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        Dim laffyTaffy As Integer
+        laffyTaffy = 26
+        Console.WriteLine(laffyTaffy)
     End Sub
 
 End Module
