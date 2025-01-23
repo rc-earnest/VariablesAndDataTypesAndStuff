eec56c46face2b666fc46fd8894a120ef91fb4d6
rc-earnest <rc.earnest04@gmail.com>
2025-01-23 08:57:46 -0700

    add data type/conversion example

--- VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
+++ VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
@@ -4,12 +4,15 @@
 'VariablesAndDataTypesAndStuff
 'https://github.com/rc-earnest/VariablesAndDataTypesAndStuff.git
 Option Explicit On
+Option Strict On
 Module VariablesAndDataTypesAndStuff
 
     Sub Main()
 
         Dim jellyBeans As Integer
         Dim laffyTaffy As Integer
+        Dim money As Decimal
+        Dim studentName As String
 
         Console.WriteLine(jellyBeans)
 
@@ -22,9 +25,16 @@ Module VariablesAndDataTypesAndStuff
         jellyBeans += 10
         Console.WriteLine(jellyBeans)
 
-        Dim laffyTaffy As Integer
         laffyTaffy = 26
         Console.WriteLine(laffyTaffy)
+
+        money = 9.21837498812374D
+        jellyBeans = CInt(1.56)
+        studentName = "7"
+
+        jellyBeans = CInt(studentName)
+
+        Console.WriteLine()
     End Sub
 
 End Module
